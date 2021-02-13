8f356073c0fd60e841f8c8c15e91b7d6576a6616
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-02-13 23:55:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown(
         f"""
 <style>
     .reportview-container .main .block-container{{
-        max-width: {max_width}px;
+        
         padding-top: {padding_top}rem;
         padding-right: {padding_right}rem;
         padding-left: {padding_left}rem;
