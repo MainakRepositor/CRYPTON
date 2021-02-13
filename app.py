5a470a5e5488ec98e44e15033a2371783f08e2f6
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-02-14 00:08:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ col2.dataframe(df_change)
 col2.subheader('Story of Cryptocurrency')
 col2.markdown('''A cryptocurrency (or crypto currency or crypto for short) is a digital asset designed to work as a medium of exchange wherein individual coin ownership records are stored in a ledger existing in a form of computerized database using strong cryptography to secure transaction records, to control the creation of additional coins, and to verify the transfer of coin ownership. It typically does not exist in physical form (like paper money) and is typically not issued by a central authority. Cryptocurrencies typically use decentralized control as opposed to centralized digital currency and central banking systems. When a cryptocurrency is minted or created prior to issuance or issued by a single issuer, it is generally considered centralized. When implemented with decentralized control, each cryptocurrency works through distributed ledger technology, typically a blockchain, that serves as a public financial transaction database ''')
 url1 = 'https://www.linkedin.com/in/mainak-chaudhuri-127898176/'
-if col1.button('Made with 💖 by Mainak Chaudhuri'):
+if col1.button('Click'):
     webbrowser.open_new_tab(url1)
 
 col2.subheader('About Blockchain')
